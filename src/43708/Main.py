72dd351b34c95b87d608b03a8407d7faeb82616c
Harry Huang <harryhuang2652@qq.com>
2024-11-22 00:20:45 +0800

    perf: 43708

--- src/43708/Main.py
+++ src/43708/Main.py
@@ -1,24 +1,25 @@
 # 43708
 
-txt = input().lower()
-codes = [input().lower() for _ in range(int(input()))]
+txt = input()
+codes = [input() for _ in range(int(input()))]
 
 CODE_LEN = 8
 LETTERS = "abcdefghijklmnopqrstuvwxyz"
 LETTERS_LEN = len(LETTERS)
+POW_TABLE = [pow(CODE_LEN, i) for i in range(LETTERS_LEN)]
 
 codes_stats = []
 for c in codes:
     codes_stats.append(0)
     for l in range(LETTERS_LEN):
-        codes_stats[-1] += c.count(LETTERS[l]) * (LETTERS_LEN ** l)
+        codes_stats[-1] += c.count(LETTERS[l]) * POW_TABLE[l]
 
 parts_stats = []
-for i in range(len(txt) + 1 - 8):
-    p = txt[i:i+8]
+for i in range(len(txt) + 1 - CODE_LEN):
+    p = txt[i:i + CODE_LEN]
     parts_stats.append(0)
     for l in range(LETTERS_LEN):
-        parts_stats[-1] += p.count(LETTERS[l]) * (LETTERS_LEN ** l)
+        parts_stats[-1] += p.count(LETTERS[l]) * POW_TABLE[l]
 
 count = 0
 for cs in codes_stats:
